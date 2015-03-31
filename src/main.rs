afcc17c1d518fff40294cda7b817ea8f2b452c41
Lewis Gunsch <lewis@gunsch.ca>
2015-03-31 14:58:42 -0600

    Utilize Paths in main method to recursively scan for paths.
    Signed-off-by: Lewis Gunsch <lewis@gunsch.ca>

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,41 @@
 
 extern crate regex;
 
-#[cfg(test)]
-extern crate hamcrest;
+#[cfg(test)] extern crate hamcrest;
 
 pub mod path;
 
+#[cfg(not(test))] use std::error::Error;
+#[cfg(not(test))] use std::fs::walk_dir;
+#[cfg(not(test))] use std::path::PathBuf;
+#[cfg(not(test))] use path::Paths;
+
 #[cfg(not(test))]
 fn main() {
-    println!("Hello, world!")
+    let mut paths = Paths::new();
+    let path_iter = match walk_dir(PathBuf::from("/var/tmp")) {
+        Ok(t) => t,
+        Err(e) => {
+            perror("cannot read directory", e);
+            return;
+        }
+    };
+    for dir_entry in path_iter {
+        match dir_entry {
+            Ok(de) => paths.add(de.path()),
+            Err(e) => pwarning("cannot add path", e),
+        }
+    }
+
+   println!("Scanned in {} paths...", paths.count());
+}
+
+#[cfg(not(test))]
+fn perror<T: Error>(msg: &str, err: T) {
+    println!("ERROR: {}: {}", msg, err);
+}
+
+#[cfg(not(test))]
+fn pwarning<T: Error>(msg: &str, err: T) {
+    println!("WARNING: {}: {}", msg, err);
 }
